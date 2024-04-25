2b70e297b20b7741e9582043560dcd4289829d21
deepak14ri <deepakchaudhari705@gmail.com>
2024-04-25 09:17:25 +0530

    assignment changes

--- client/src/utils/handleError.js
+++ client/src/utils/handleError.js
@@ -1,5 +1,5 @@
 import { toast } from "react-toastify";
-
+//Error handler
 export const handleError = (error) => {
   console.log(error?.response?.data?.message);
   toast.error(error?.response?.data?.message);
